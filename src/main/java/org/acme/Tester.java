458a34fbfec12892ceccb1d4c6fae276e6431ad6
rlj <rlj@convergens.dk>
2020-01-30 15:35:11 +0100

    works with new dependencies (1.2.12)

--- src/main/java/org/acme/Tester.java
+++ src/main/java/org/acme/Tester.java
@@ -6,6 +6,7 @@
 package org.acme;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import java.io.IOException;
@@ -19,8 +20,7 @@ import java.util.Scanner;
  * @author Rasmu
  */
 public class Tester {
-/*
-    Gson gson = new Gson();
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     public String getHttpData(String url) throws MalformedURLException, IOException {
         URL getUrl = new URL(url);
@@ -45,6 +45,7 @@ public class Tester {
         companyInfo.add("zipcode", gson.toJsonTree(cvrJson.get("zipcode")));
         companyInfo.add("city", gson.toJsonTree(cvrJson.get("city")));
         companyInfo.add("phone", gson.toJsonTree(cvrJson.get("phone")));
+        companyInfo.add("email", gson.toJsonTree(cvrJson.get("email")));
         
         
         System.out.println(companyInfo);
@@ -56,5 +57,5 @@ public class Tester {
         test.tester();
 
     }
-    */
+    
 }
